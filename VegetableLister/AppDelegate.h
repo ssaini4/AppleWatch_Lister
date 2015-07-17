aa58ba3deaf551bae4e461a216d10034d4e2dd2d
ssaini4 <ssaini4@illinois.edu>
2015-07-16 20:57:43 -0500

    Initial Commit

---
+++ VegetableLister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VegetableLister
+//
+//  Created by Saksham Saini on 7/16/15.
+//  Copyright (c) 2015 Saksham Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
